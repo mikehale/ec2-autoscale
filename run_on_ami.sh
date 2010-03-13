c9387f486406a089b85bc5982c834e12d28af4bf
Michael Hale <mikehale@gmail.com>
2010-03-12 22:44:29 -0500

    add example of extracting host key fingerprints

--- run_on_ami.sh
+++ run_on_ami.sh
@@ -49,6 +49,7 @@ ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N '' | logger -s -t "ec2"
 ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N '' | logger -s -t "ec2"
 
 # This allows user to get host keys securely through console log
+# For example: ec2-get-console-output <instanceid> |grep "BEGIN SSH HOST KEY" -A 2|tail -n2|awk '{ print $3 }'
 echo "-----BEGIN SSH HOST KEY FINGERPRINTS-----" | logger -s -t "ec2"
 ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub | logger -s -t "ec2"
 ssh-keygen -l -f /etc/ssh/ssh_host_dsa_key.pub | logger -s -t "ec2"
